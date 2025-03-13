40a2dc29d61aef5453742a4d531cc7ac1dd1308a
Hafsa Sheikh <sendtohafsa0345@gmail.com>
2025-03-14 02:15:30 +0500

    psm.py

--- psm.py
+++ psm.py
@@ -7,7 +7,7 @@ st.markdown("""
 <style>
     .main {text-align: center";}
     .stTextInput {width: 60% !important margin: auto; }
-    .stButton button {width: 50%; background-color; blue color: white; font-size: 18px; }
+    .stButton button {width: 50%; background-color; blue; color: white; font-size: 18px; }
     .stButton button:hover {background-color: red; color:white;}
     </style>
     """, unsafe_allow_html=True)
